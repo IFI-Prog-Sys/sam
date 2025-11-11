da223bb17b28846d5fee088f322ce05f5421ae26
Evgeny Pivovarskiy <pivovarskiy.evgeny@gmail.com>
2025-11-12 00:44:43 +0100

    Add get latest events functionality

--- sam.py
+++ sam.py
@@ -3,36 +3,67 @@ from enum import Enum
 from bs4 import BeautifulSoup, Tag
 import requests
 import json
-import datetime
+from datetime import datetime, timezone
 
 FIVE_SECONDS = 5
+
+class Comparison(Enum):
+    LEFT_GREATER = -1
+    EQUAL = 0
+    RIGHT_GREATER = 1
+
 
 class SamError(Enum):
     HTTP = 1
     UNKNOWN = 2
     METADATA_NOT_FOUND = 3
     NOT_A_TAG = 4
+    JSON_CONVERSION = 5
 
 @dataclass
 class Event:
     title: str
     description: str
-    datetime: datetime.datetime
+    datetime: datetime
     place: str
     link: str
 
 class Sam():
     def __init__(self, peoply_organization_name):
-        self._pending_events: list[Event] = list()
-        self._organization_page_endpoint = f"https://peoply.app/orgs/{peoply_organization_name}"
-        self._header = {
+        self._http_header = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36"
         }
+        self._api_header = {
+            "Accept": "application/json",
+            "User-Agent": "SamTheScraper/0.5 (+https://progsys.no)"
+        }
+
+        self._organization_page_endpoint = f"https://peoply.app/orgs/{peoply_organization_name}"
+        self._organization_uuid = self.__updateOrganizationUuid()
+        print(f"Sam: Set self. value to {self._organization_uuid}")
+
+        self._pending_events: list[Event] = list()
+        self._last_update = self.__get_curent_formatted_time()
+
+    def __get_curent_formatted_time(self):
+        current_utc_time = datetime.now(timezone.utc)
+        formatted_time = current_utc_time.isoformat(timespec="milliseconds").replace("+00:00", "Z")
+        return formatted_time
+    
+    def __compare_time(self, left, right) -> Comparison:
+        left_parsed = datetime.fromisoformat(left)
+        right_parsed = datetime.fromisoformat(right)
+        if left_parsed < right_parsed:
+            return Comparison.RIGHT_GREATER
+        elif left_parsed > right_parsed:
+            return Comparison.LEFT_GREATER
+        else:
+            return Comparison.EQUAL
 
     def __get_raw_organization_page(self) -> str | SamError:
         try:
             print(f"Sam: Sending request to {self._organization_page_endpoint}")
-            response = requests.get(self._organization_page_endpoint, headers=self._header ,timeout=FIVE_SECONDS)
+            response = requests.get(self._organization_page_endpoint, headers=self._http_header ,timeout=FIVE_SECONDS)
             response.raise_for_status()
         except requests.exceptions.HTTPError as error:
             print(f"Sam: Request all events FAIL | HTTP error {error}")
@@ -113,6 +144,79 @@ class Sam():
         print(f"Sam: Organization id: {org_uuid}")
 
         return org_uuid
+    
+    def __get_latest_raw_events(self):
+        api_endpoint = f"https://api.peoply.app/events?afterDate={self._last_update}&organizationId={self._organization_uuid}"
 
-    def getLatestEvents(self):
+        try:
+            api_request_response = requests.get(api_endpoint, headers=self._api_header, timeout=FIVE_SECONDS)
+            api_request_response.raise_for_status()
+            json_data = api_request_response.json()
+            return json_data
+        except requests.exceptions.HTTPError as error:
+            print(f"Sam: Request API endpoint FAIL | HTTP error {error}")
+            print(f"Sam: -> Status code: {error.response.status_code}")
+            return SamError.HTTP
+        except requests.exceptions.RequestException as error:
+            print(f"Sam: Request API endpoint FAIL | Unknown error")
+            return SamError.UNKNOWN
+        except ValueError as error:
+            print(f"Sam: Parson API reply to JSON FAIL | Unknown error")
+            return SamError.JSON_CONVERSION
+        
+    def __safe_json_get(self, attribute, json_file) -> str:
+        fetched_attribute = json_file.get(attribute)
+        if fetched_attribute is None:
+            return "null"
+        else:
+            return fetched_attribute
+        
+    def __parse_raw_event_data(self, raw_event_json) -> Event:
+        title = self.__safe_json_get("title", raw_event_json)
+        description = self.__safe_json_get("description", raw_event_json)
+        start_date = self.__safe_json_get("startDate", raw_event_json)
+        place = self.__safe_json_get("locationName", raw_event_json)
+        link_id = self.__safe_json_get("urlId", raw_event_json)
+
+        event = Event(
+            title = title,
+            description = description,
+            datetime = datetime.fromisoformat(start_date),
+            place = place,
+            link = f"https://peoply.app/events/{link_id}"
+        )
+
+        return event
+    
+    def __update_sam_events_list(self):
+        get_events_response = self.__get_latest_raw_events()
+
+        match get_events_response:
+            case SamError.HTTP:
+                print(f"Sam: Update events list FAIL | HTTP error. Not commiting to update.")
+                return
+            case SamError.UNKNOWN:
+                print(f"Sam: Update events list FAIL | UKNOWN network error. Not commiting to update.")
+                return
+            case SamError.JSON_CONVERSION:
+                print(f"Sam: Update events list FAIL | JSON_CONVERSION error. Not commiting to update.")
+                return
+            case dict() as d:
+                event = self.__parse_raw_event_data(get_events_response)
+                self._pending_events.append(event)
+            case list() as l:
+                for raw_event in get_events_response:
+                    event = self.__parse_raw_event_data(raw_event)
+                    self._pending_events.append(event)
+            case _:
+                print(f"Sam: Unknown case occured in __update_sam_events_list(). Panic! Exiting...")
+                exit(1)
+
+        self._last_update = self.__get_curent_formatted_time()
+        print(f"Sam: Pending events: {self._pending_events}")
+
+    def updateLatestEvents(self):
+        self.__update_sam_events_list()
+
+    def extractLatestEvents(self) -> list[Event]:
         return self._pending_events
